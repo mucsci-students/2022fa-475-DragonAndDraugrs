5b5476259d3995d793b3dedf603003dfd59ea6bc
logan-eller <logan.eller@gmail.com>
2022-10-24 23:44:36 -0400

    add collider to rocks in hub

--- Assets/Scripts/AnimationControl.cs
+++ Assets/Scripts/AnimationControl.cs
@@ -7,17 +7,14 @@ public class AnimationControl : MonoBehaviour
     public GameManager GameManager;
     public Transform Player;
     int MoveSpeed = 4;
-    float Force = 15f;
     int MaxDist = 20;
     public Rigidbody rigidbody;
     Animator anim;
     public Vector3 playerDirection;
     // Start is called before the first frame update
-
     void Start()
     {
         anim = GetComponent<Animator>();
-        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -25,19 +22,24 @@ public class AnimationControl : MonoBehaviour
     {
 
 
+
+        rigidbody = GetComponent<Rigidbody>();
+        //waterBody.velocity = new Vector3(0, 0.5f, 0);
         if (Vector3.Distance(transform.position, Player.position) < MaxDist)
         {
+            playerDirection = Player.transform.position - gameObject.transform.position;
             transform.LookAt(Player);
-            //
+            //rigidbody.velocity = new Vector3(MoveSpeed, 0, 0);
+            //transform.rotation = Quaternion.Slerp(transform.rotation
+                                       //, Quaternion.LookRotation(Player.position - transform.position)
+                                       //, 3f * Time.deltaTime);
 
             /* Move at Player*/
-            //playerDirection = Player.transform.position - gameObject.transform.position;
-            //transform.position = transform.position + new Vector3(playerDirection.x * MoveSpeed * Time.deltaTime, playerDirection.y * MoveSpeed * Time.deltaTime, playerDirection.z * MoveSpeed * Time.deltaTime);
+            transform.position = transform.position + new Vector3(playerDirection.x * MoveSpeed * Time.deltaTime, playerDirection.y * MoveSpeed * Time.deltaTime, playerDirection.z * MoveSpeed * Time.deltaTime);
             //transform.position = Vector3.MoveTowards(transform.position, Player.position, MoveSpeed * Time.deltaTime);
             //transform.position += transform.forward * MoveSpeed * Time.deltaTime;
-            //rigidbody.MovePosition(transform.position + transform.forward * MoveSpeed);
-            rigidbody.AddForce(transform.forward * Force);
             anim.SetFloat("speed", MoveSpeed);
+            //rigidbody.MovePosition(transform.position + transform.forward * MoveSpeed);
 
         }
         else
