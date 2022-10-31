61c522dfc4ac5ebd068414c92363a3206b76b291
Sh3p <chsheppe@millersville.edu>
2022-10-31 15:28:31 -0400

    added gold

--- Assets/Scripts/ScripatableObjects/ItemObject.cs
+++ Assets/Scripts/ScripatableObjects/ItemObject.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0772f921091c6d3cc02196fed5579fb7a800d151c3f386fc965c90c99cdf1ea0
-size 308
+oid sha256:8656ccf8cdb28adf05aea94d16493257357343fa5b29dd66201e5e49a86cdbe2
+size 318
