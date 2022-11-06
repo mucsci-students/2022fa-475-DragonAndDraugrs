45163616bebb3ade29e6e34b55c53e95aad9df62
Sh3p <chsheppe@millersville.edu>
2022-11-06 18:56:23 -0500

    chaneges to level2

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d6ae5469709084c450750ded8cddc170653fea88328e02cda944df84d7096e6
-size 1179
+oid sha256:551026c49d7e09944be723428d985b45a8129662ddba425fd454b1802d1ae2a7
+size 1248
