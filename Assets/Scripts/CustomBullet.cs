aa36db8088252b35aabda36d423e527aa65284b6
noahbarger <98166939+noahbarger@users.noreply.github.com>
2022-11-06 20:08:09 -0500

    nw yellow gun

--- Assets/Scripts/CustomBullet.cs
+++ Assets/Scripts/CustomBullet.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00b7905ba0b1f3d58ff6c2dddb9e9efe51cb5cc78d6230af4dcdbedcedc9f1cf
-size 2888
+oid sha256:1e7986b60a7472fd074f1cb82ec11975cd3652fb074ca7df90aa4e3a6b166c56
+size 2890
