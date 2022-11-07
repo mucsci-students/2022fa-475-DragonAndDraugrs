711284d7e3dc0b5eb6d8badc01efa94f608c14da
noahbarger <98166939+noahbarger@users.noreply.github.com>
2022-11-06 23:39:58 -0500

    boss room gun

--- Assets/SwordController.cs
+++ Assets/SwordController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc46f7d41458a20afac79790d3f881f8ec58c8e60512de37621c931056086298
-size 1234
+oid sha256:f33ff8c15b0630a5fb7aea207add0fdbfdab3de3c383d8ce94392c4f020e6c9c
+size 1157
