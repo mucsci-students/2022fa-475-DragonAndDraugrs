8e66422f40acfd88b450dafdbcb3f2a7dda53fe4
logan-eller <logan.eller@gmail.com>
2022-11-06 22:28:26 -0500

    update main menu and add script to main boss

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9d4bdbc1f7f83d5221b92c4d38f5a2627216763f7bf1833bdaecff9e7e90592
-size 9437
+oid sha256:9ff411cd7b2c98dd72a4f6ae58c1d443ea9971e94ae934964d618d978191fd03
+size 10010
